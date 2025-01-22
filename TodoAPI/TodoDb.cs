3bffb676bf5d2e200494dd50e45e49d9e1b2daa6
thienhuong320 <phantranthienhuong320@gmail.com>
2025-01-23 00:55:04 +0700

    up

---
+++ TodoAPI/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
